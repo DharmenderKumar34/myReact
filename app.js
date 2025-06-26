08acd534c8b80eb66a18e691634db0cfd5ab56db
Dharmender_kumar <dharmenderkumar3465@gmail.com>
2025-06-26 09:58:31 +0530

    new content

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
  import React from "react";
  import ReactDOM from "react-dom/client";
    console.log("hello verma");
-    const heading=(
+    const Heading=()=>(
        <h1
       id="title" key="h">
       Nameste react
       </h1>
     )
+    const Headercomponent=()=>{
+      return(
+        <div>
+          {<Heading/>}
+          <h1>Nameste react by Akshay saini</h1>
+          <h2>This is h2 tag</h2>
+          
+        </div>
+      )
+    }
+
+    
       let root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(heading);
+      // root.render([Heading]);
+      root.render(<Headercomponent/>);
