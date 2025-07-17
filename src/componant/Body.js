8141e74231e767048baebc4d0d3d3e353fff7580
Dharmender_kumar <dharmenderkumar3465@gmail.com>
2025-07-17 22:25:56 +0530

    food villa

--- src/componant/Body.js
+++ src/componant/Body.js
@@ -1,13 +1,29 @@
-import React from "react";
-import {Restaurantlist} from "../constants"
-import Restrauntcard from "./Restrauntcard"
+import React, { useState } from "react";
+import { Restaurantlist } from "../constants";
+import Restrauntcard from "./Restrauntcard";
+// import {useState} from "react";
 const Body = () => {
+  const [searchText, setsearchinput] = useState("kfc");
   return (
-    <div className="rest">
-      {Restaurantlist.map((restaurant, index) => (
-        <Restrauntcard restaurant={restaurant.info} key={index} />
-      ))}
-    </div>
+    <React.Fragment>
+      <div>
+        <input
+          type="text"
+          placeholder="search"
+          value={searchText}
+          onChange={(e) => {
+            setsearchinput(e.target.value);
+          }}
+        />
+        <button className="searchbtn">Search-{searchText}</button>
+      </div>
+      
+      <div className="rest">
+        {Restaurantlist.map((restaurant, index) => (
+          <Restrauntcard restaurant={restaurant.info} key={index} />
+        ))}
+      </div>
+    </React.Fragment>
   );
 };
-export default Body
+export default Body;
