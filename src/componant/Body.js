21b1f83bfacb741ccaff3f0104e2de89a101d140
Dharmender_kumar <dharmenderkumar3465@gmail.com>
2025-07-19 16:14:51 +0530

    add content

--- src/componant/Body.js
+++ src/componant/Body.js
@@ -5,13 +5,18 @@ import Restrauntcard from "./Restrauntcard";
 const Body = () => {
   const [searchText, setsearchinput] = useState("");
   const [resto, setsearchclick] = useState(Restaurantlist);
+  // console.log("render")
   const filterData = () => {
-    const rest=Restaurantlist.filter(res=>{
-      return res.info.name.toLocaleLowerCase()
-      .includes(searchText.toLocaleLowerCase())
-    })
-    setsearchclick(rest)
-  }
+    console.log("restrunt", resto);
+    const rest = Restaurantlist.filter((res) => {
+      return res.info.name
+        .toLocaleLowerCase()
+        .includes(searchText.toLocaleLowerCase());
+    });
+    console.log(rest);
+
+    setsearchclick(rest);
+  };
   return (
     <React.Fragment>
       <div>
@@ -23,14 +28,18 @@ const Body = () => {
             setsearchinput(e.target.value);
           }}
         />
-        
+
         {/* <h1>{searchClicked}</h1> */}
-        <button className="searchbtn"
-        onClick={() => {
+        <button
+          className="searchbtn"
+          onClick={() => {
             filterData();
-          }}>Search</button>
+          }}
+        >
+          Search
+        </button>
       </div>
-      
+
       <div className="rest">
         {resto.map((restaurant, index) => (
           <Restrauntcard restaurant={restaurant.info} key={index} />
