a01848458dad14662dff99c2d5fef124235caf21
Dharmender_kumar <dharmenderkumar3465@gmail.com>
2025-07-31 09:35:49 +0530

    rauter

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./componant/Header"
-import Body from "./componant/Body"
+import Header from "./componant/Header";
+import Body from "./componant/Body";
+import About from "./componant/About";
+import Error from "./componant/Error";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const AppLayout = () => {
   return (
@@ -11,5 +14,17 @@ const AppLayout = () => {
     </React.Fragment>
   );
 };
+
+const Approuter = createBrowserRouter([
+  {
+    path: "/",
+    Component: AppLayout,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    Component: About,
+  },
+]);
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={Approuter} />);
--- src/componant/Header.js
+++ src/componant/Header.js
@@ -1,24 +1,19 @@
 import React, { useState } from "react";
-// import IMG from "../assets/img/Foodvilla.png";
-// const loggedIn = () => {
-//   return false;
-// };
-
+import { Link } from "react-router";
 const Header = () => {
-const [login,setlogin]=useState(true)
-const handlelogin=()=>{
-  setlogin(!login)
-}
+  const [login, setlogin] = useState(true);
+  const handlelogin = () => {
+    setlogin(!login);
+  };
   return (
-
     <div className="nav-list">
       <div>
-        {/* <a href="/"> */}
+        <a href="/">
           <img
-            src={"/lpu.webp"}
+            src="https://content3.jdmagicbox.com/comp/rohtak/j1/9999p1262.1262.230322230029.w5j1/catalogue/food-villa-family-restaurant-and-hotel-rohtak-restaurants-vepedpc3pq.jpg"
             className="logo"
           />
-        {/* </a> */}
+        </a>
       </div>
       <div
         style={{
@@ -26,8 +21,12 @@ const handlelogin=()=>{
         }}
       >
         <ul className="nav-items">
-          <li>Home</li>
-          <li>About</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
           <li>Contact</li>
           <li>Cart</li>
         </ul>
@@ -35,7 +34,7 @@ const handlelogin=()=>{
       {/* {loggedIn() ? <button>Logout</button> : <button onClick={
 
       }>Login</button>} */}
-      <button onClick={handlelogin}>{login?"login":"logout"}</button>
+      <button onClick={handlelogin}>{login ? "login" : "logout"}</button>
     </div>
   );
 };
